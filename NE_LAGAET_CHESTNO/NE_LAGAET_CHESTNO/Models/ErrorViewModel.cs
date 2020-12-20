7a3b715c093b0210202cbf128ed091bdb5a343c3
Stepan Aracelyantc <asv.17@uni-dubna.ru>
2020-12-20 20:27:54 +0300

    вот теперь добавил mvc проект

---
+++ NE_LAGAET_CHESTNO/NE_LAGAET_CHESTNO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NE_LAGAET_CHESTNO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
