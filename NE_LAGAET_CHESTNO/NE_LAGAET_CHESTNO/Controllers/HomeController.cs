13c9815da0ad1e4a2b981c39ddf83aa5bd42f02f
Stepan Aracelyantc <asv.17@uni-dubna.ru>
2020-12-25 01:47:41 +0300

    добавил ajax в  проект

--- NE_LAGAET_CHESTNO/NE_LAGAET_CHESTNO/Controllers/HomeController.cs
+++ NE_LAGAET_CHESTNO/NE_LAGAET_CHESTNO/Controllers/HomeController.cs
@@ -204,6 +204,7 @@ namespace NE_LAGAET_CHESTNO.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- NE_LAGAET_CHESTNO/NE_LAGAET_CHESTNO/wwwroot/js/site.js
+++ NE_LAGAET_CHESTNO/NE_LAGAET_CHESTNO/wwwroot/js/site.js
@@ -1,4 +1,55 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function Search() {
+    $.ajax({
+        type: "PosT",
+        url: "/Home/AdvertSearch",
+        data: {
+            "Id": document.getElementById("Id").value,
+            "request": document.getElementById("request").value,
+        },
+        success: function (response) {
+            if (response.isRedirect) {
+                window.location.href = response.redirectUrl;
+            }
+            const adverts = document.getElementById("adverts");
+            while (adverts.lastElementChild)
+                adverts.removeChild(adverts.lastElementChild)
+            var cases = [2, 0, 1, 1, 1, 2];
+            var Monthes = ["месяц", "месяца", "месяцев"];
+            response.forEach(function (data, i, response) {
+                var advert = document.createElement("div");
+                advert.className = "advert";
+                var month = Monthes[data.timeOfUse % 100 > 4 && data.timeOfUse % 100 < 20 ? 2 : cases[(advert.timeOfUse % 10 < 5) ? data.timeOfUse % 10 : 5]];
+                if (data.other != null) {
+                    advert.innerHTML = `<div class="advert_title">` +
+                        `<p>МЕНЯЮ: ` + data.phone.brand + ` ` + data.phone.model + ` НА: ` + data.replacementPhone + `</p>` +
+                        `</div>` +
+                        `<div class="advert_description">` +
+                        `<p>ОС: ` + data.phone.os + `, прошивка: ` + data.phone.firmware + `, дисплей: ` + data.phone.display + `, ёмкость батареи: ` + data.phone.batteryCapacity + ` mAh, мощность зарядки: ` + data.phone.chargePower + ` Вт, ОЗУ: ` + data.phone.ram + ` GB, ПЗУ: ` + data.phone.rom + ` GB, цвет: ` + data.phone.color + `</p>` +
+                        `<p>Прочее: ` + data.other + `</p>` +
+                        `<p>Время пользования: ` + data.timeOfUse + ` ` + month + `</p>` +
+                        `<button id="response"><a href="/Home/Advertisement_Page/` + data.id + `">Откликнуться</a></button>` +
+                        `</div>`
+                }
+                else {
+                    advert.innerHTML = `<div class="advert_title">` +
+                        `<p>МЕНЯЮ: ` + data.phone.brand + ` ` + data.phone.model + ` НА: ` + data.replacementPhone + `</p>` +
+                        `</div>` +
+                        `<div class="advert_description">` +
+                        `<p>ОС: ` + data.phone.os + `, прошивка: ` + data.phone.firmware + `, дисплей: ` + data.phone.display + `, ёмкость батареи: ` + data.phone.batteryCapacity + ` mAh, мощность зарядки: ` + data.phone.chargePower + ` Вт, ОЗУ: ` + data.phone.ram + ` GB, ПЗУ: ` + data.phone.rom + ` GB, цвет: ` + data.phone.color + `</p>` +
+                        `<p>Время пользования: ` + data.timeOfUse + ` ` + month + `</p>` +
+                        `<button id="response"><a href="/Home/Advertisement_Page/` + data.id + `">Откликнуться</a></button>` +
+                        `</div>`
+                }
+                adverts.appendChild(advert);
+            });
+        },
 
-// Write your JavaScript code.
+        failure: function (response) {
+
+        },
+
+        error: function (response) {
+
+        }
+    });
+}
